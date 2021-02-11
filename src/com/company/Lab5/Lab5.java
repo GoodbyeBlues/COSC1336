9bb7c3a4c2b0d7ddceee73cdeb3d1ca1089d216b
GoodbyeBlues <elizabeth.graham2000@gmail.com>
2021-02-10 19:48:50 -0600

    Completed Lab5

--- src/com/company/Lab5/Lab5.java
+++ src/com/company/Lab5/Lab5.java
@@ -2,7 +2,6 @@ package com.company.Lab5;
 
 import java.awt.*;
 import java.util.Scanner;
-import java.util.Random;
 
 
 public class Lab5 {
@@ -15,7 +14,6 @@ public class Lab5 {
     public static final int PANEL_SIZE = 512;  // Constant panel size var, width and height
     public static DrawingPanel panel = new DrawingPanel(PANEL_SIZE, PANEL_SIZE);  // Init Panel with proper dimensions
     public static Graphics g = panel.getGraphics();  // Init graphics instance
-    public static Random rand = new Random(); // Create random instance
 
 
     public static void main(String[] args) {
@@ -29,9 +27,15 @@ public class Lab5 {
         
         Scanner scanner = new Scanner(System.in);  // Init scanner, for input
 
-        System.out.println("Circle Drawing Program\n");
+        System.out.println("Circle Drawing Program\n");  // Output the name of the program
 
+        // Each circle will be colored in the order it's made, so R => G => B
         Color[] colorArray = {Color.RED,Color.GREEN,Color.BLUE};
+
+        // Arrays to store the radius and center information
+        // Will use when comparing sizes and determining intersections
+        int[] circleRadii = new int[3];
+        int[] circleCenters = new int[3];
 
         for (int i = 0; i < 3; i++) {
 
@@ -39,32 +43,84 @@ public class Lab5 {
             String center = scanner.nextLine();
 
             System.out.println("Enter radius: ");
-            String radius = scanner.nextLine();     
+            String radius = scanner.nextLine();    
+            
+            circleRadii[i] = Integer.parseInt(radius);  // Add the current radius to the radii array
+            circleCenters[i] = Integer.parseInt(center); // Add the current center point to the centers array
 
-            g.setColor(colorArray[i]);
+            g.setColor(colorArray[i]);  // Set the color of the circle to the current indice
 
 
-            drawCircle(findCenterPoint(Integer.parseInt(center), Integer.parseInt(radius)), Integer.parseInt(radius));
+            drawCircle(findCenterPoint(Integer.parseInt(center), Integer.parseInt(radius)), Integer.parseInt(radius));  // Call the draw circle function using center (using findCenter func) and radius
+        }
+        scanner.close();  // Close scanner
+
+        // Output: The green circle is {smaller than / larger than / the same size as} the red circle.
+        System.out.println("\nCompare Sizes");
+        System.out.println("The red circle is " + compareSizeString(compareSizes(circleRadii[0], circleRadii[1])) + " the green circle");
+        System.out.println("The red circle is " + compareSizeString(compareSizes(circleRadii[0], circleRadii[2])) + " the blue circle");
+        System.out.println("The green circle is " + compareSizeString(compareSizes(circleRadii[1], circleRadii[2])) + " the blue circle");
+
+        // Output: The green circle and the blues circle {intersect / do not intersect}.
+        System.out.println("\nDo the circles intersect");
+        System.out.println("The red and green circles " + doesIntersectString(doesIntersect(circleRadii[0], circleRadii[1], circleCenters[0], circleCenters[1], circleCenters[0], circleCenters[1])));
+        System.out.println("The red and blue circles " + doesIntersectString(doesIntersect(circleRadii[0], circleRadii[2], circleCenters[0], circleCenters[2], circleCenters[0], circleCenters[2])));
+        System.out.println("The green and blue circles" + doesIntersectString(doesIntersect(circleRadii[1], circleRadii[2], circleCenters[1], circleCenters[2], circleCenters[1], circleCenters[2])));
+    }
+
+
+    public static String compareSizeString(int boolVal) {
+        /*
+            Convert the value passed from compareSizes to english
+        */
+        if (boolVal == 1) {
+            return "larger than";
         }
 
-        // Output: The green circle is smaller than the red circle.
+        else if (boolVal == -1) {
+            return "smaller than";
+        }
 
-        scanner.close();  // Close scanner
+        else {return "the same size as";}
+    }
+
+
+    public static String doesIntersectString(int boolVal) {
+        /*
+            Convert the value passed from doesIntersect to english
+        */
+        if (boolVal == 1) {
+            return "intersect";
+        }
+
+        else if (boolVal == -1) {
+            return "do not intersect";
+        }
+
+        else {return "you broke it";}
     }
 
 
     public static int findCenterPoint(int center, int w) {
+        /*  
+            Find the correct x,y points to have a circle with
+            the center point given by the user. 
+        */
         double xPoint = center - (0.5 * w);  // x = center - 1/2w
         return (int)xPoint;
     }
 
 
     public static void drawCircle(int xPoint, int radius) {
+        /* 
+            Call the fillOval function using the arguments xPoint 
+            for coordinates and radius for radius.
+        */
         g.fillOval(xPoint, xPoint, radius, radius);
     }
 
 
-    public static void compareSizes(int r1, int r2) {
+    public static int compareSizes(int r1, int r2) {
         /*
             A static method named compareSizes(...) should have parameters for the radii of
             two circles (two parameters), and the method should return -1 if the first circle is
@@ -73,10 +129,17 @@ public class Lab5 {
             print a line of output like:
             The green circle is smaller than the red circle.
         */
+        if (r1 > r2) {
+            return 1;  // If r1 is larger
+        }
+
+        else if (r1 < r2) {
+            return -1;  // If r1 is smaller
+        } else {return 0;}  // If both are the same saze
     }
 
 
-    public static void doesIntersect(int r1, int r2, int x1, int x2, int y1, int y2) {
+    public static int doesIntersect(int r1, int r2, int x1, int x2, int y1, int y2) {
         /*
             Another static method named doesIntersect(...) should have parameters for the
             radii and locations of two circles (six parameters), and the method should return 1
@@ -90,12 +153,22 @@ public class Lab5 {
             main method should print a line of output like:
             The blue circle intersects the red circle.
         */
+
+        // They do intersect if the sum of the distances is less than the sum of the radii
+        if ((Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)) < Math.pow(r1+r2, 2)) {
+            return 1; // Intersects
+        }
+
+        // They don't intersect if the sum of the distances is greater than the sum of the radii
+        else if ((Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)) > Math.pow(r1+r2, 2)) {
+            return -1; // Doesnt intersect
+        } else {return 0;}  // All code paths return value
     }
 
 
     public static void isCircleInWindow() {
         /*
-            Given a circle, return 1if the circle is completely within the window,
+            *OPTIONAL* Given a circle, return 1 if the circle is completely within the window,
             return 0 if the circle is partially within the window, or return -1 if the circle is
             completely outside the window. The program should print something appropriate. 
         */
