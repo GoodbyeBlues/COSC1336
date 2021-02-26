dd5389a2c216e8ddf697661fe183f6e522ba5d4b
GoodbyeBlues <elizabeth.graham2000@gmail.com>
2021-02-26 10:43:16 -0600

    Began work on Project1

---
+++ Project1/Patterns.java
@@ -0,0 +1,58 @@
+package Project1;
+
+import java.util.Scanner;
+
+
+public class Patterns {
+    public static void main(String[] args) {
+        System.out.println("Lab 7 written by Elizabeth Graham\n");  // Output student name
+
+        Scanner scanner = new Scanner(System.in);  // Init scanner, for input
+    }
+
+
+    public static void printBoxes() {
+        /**
+         * Print Multiple Boxes
+         * 
+         * Expected Output: 
+         * +------+------+------+
+           |      |      |      |
+           |      |      |      |
+           +------+------+------+
+         */
+        
+    }
+
+
+    public static void drawBox() {
+        /** Draw Singular Box (using out.print, to allow for multiple boxes on one line)
+         * 
+         * Expected Output: 
+         * +------+
+           |      |
+           |      |
+           +------+
+         */
+    }
+
+
+    public static void drawPattern() {
+        /** Draw pattern according to user given arguments
+         * 
+         * Arguments: Width of Pattern = 3; Height of pattern = 2;
+         * Expected Output: 
+         *   X   X   X
+            / \ / \ / \
+            / \ / \ / \
+            \ / \ / \ /
+            \ / \ / \ /
+             X   X   X
+            / \ / \ / \
+            / \ / \ / \
+            \ / \ / \ /
+            \ / \ / \ /
+             X   X   X
+         */
+    }
+}
